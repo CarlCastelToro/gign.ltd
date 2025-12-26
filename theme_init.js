67bd5fa31f4a1ce65604db1c4082f7510cf57d01
Carl Castel Toro <carlcasteltoro@gmail.com>
2025-12-26 23:05:59 +0800

    Small Content Update

--- theme_init.js
+++ theme_init.js
@@ -60,7 +60,7 @@
 
     function initFirstVisit(){
         if(firstvisit){
-            showCenterAlert('欢迎来到gign.ltd, 这是你在我们质量更新后首次来访呢\n我们推荐你关闭例如Dark Reader等浏览器插件, 使用我们内置的主题样式切换开关\n它位于目录标题旁');
+            showCenterAlert('欢迎来到gign.ltd, 这是你在我们质量更新后首次来访呢\n我们推荐你关闭例如Dark Reader等浏览器插件, 使用我们内置的主题样式切换开关\n它位于目录底部');
             showCenterAlert('默认为深色样式, 你可以通过目录底部的开关随时切换它\n当目录过长时, 你可以通过滚动目录来查看所有内容', {icon: '?'});
             localStorage.setItem('siteTheme', 'dark');
             localStorage.setItem('siteFont', 'yahei'); // 新增：默认保存雅黑字体
